2ba512c759667e659a548e5ee7aa729204aa0737
Dmitry Vasiliuk <usy222@mail.ru>
2023-02-17 00:51:38 +0300

    add Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа, которая на вход принимает трехзначное число, и
+// на выходе показывает вторую цифру!
+
+Console.WriteLine("Введите трехзначное число: ");
+int.TryParse(Console.ReadLine(), out int first);
+int length = first.ToString().Length;
+int a;
+int b;
+
+if (length == 3)
+{
+  a = first / 10;
+  b = a % 10;
+  Console.WriteLine($"Вы ввели трёхзначное число: {first}, вторая цифра которого = {b}.");
+}
+else
+{
+  Console.WriteLine("Вы ввели не трёхзначное число!");
+}
