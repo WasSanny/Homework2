d8ccca5c60e980b40485eceda1ebb07e050229be
Dmitry Vasiliuk <usy222@mail.ru>
2023-02-17 23:12:38 +0300

    add Вывод Третьей Цифры Числа

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа, которая выводит третью цифру заданного числа, или
+// сообщает, что третьей цифры нет!
+
+Console.WriteLine("Введите число: ");
+int.TryParse(Console.ReadLine(), out int first);
+int length = first.ToString().Length;
+int a;
+
+if (length == 3)
+{
+  a = first % 10;
+    Console.WriteLine($"Третья цифра числа: {first} равна {a}");
+}
+if (length < 3)
+{
+  Console.WriteLine($"Третьей цифры числа {first} НЕТ!");
+}
+if (length > 3)
+{
+    Console.WriteLine($"Третьей цифрой числа {first} является {first.ToString()[2]}");
+}
